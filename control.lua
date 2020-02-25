4dd3b12ad2a0619cf38754cd3de36f7f31f1fc68
Anton Kulikov <axtoox@protonmail.com>
2020-02-26 02:22:02 +0300

    Build and Remove functions added for pixels Main features: - Add/Remove tile under pixel - Add/Remove pixel to the global table - Using 'hidden' tiles to store previous tile

--- control.lua
+++ control.lua
@@ -1,15 +1,43 @@
+Surface = nil
+Pixels = nil
 
 --- Event handler
 -- Starts update 
 -- @param event Event handlers table
 function on_pixel_built(event) 
-    game.print(event.created_entity.unit_number)
-    global.all_pixels[global.current_surface][event.created_entity.unit_number] = event.created_entity
+    --make pixel reference
+    local pixel = {
+        tile = {
+            name = "pixel-default"
+        },
+        diode = event.created_entity
+    }
+
+    Surface.set_tiles({{position = pixel.diode.position, name = pixel.tile.name}}, true)
+
+    Pixels[pixel.diode.unit_number] = pixel
+end
+
+function on_pixel_removed(event)
+
+    local original_tile_name = Surface.get_hidden_tile(event.entity.position)
+
+    Surface.set_tiles({{position = event.entity.position, name = original_tile_name}}, true)
+
+    Surface.set_hidden_tile(event.entity.position, nil)
+
+    Pixels[event.entity.unit_number] = nil
 end
 
 function on_regular_check(event)
-    if event.tick % 15 == 0 then
-        game.print(serpent.line(global.all_pixels[global.current_surface])) 
+    if event.tick % 60 == 0 then
+        game.print(serpent.line(Pixels)) 
+        --for _,pixel in pairs(Pixels) do
+            --game.print(Surface.get_hidden_tile(pixel.position))
+        --end
+        --game.print(Surface.get_hidden_tile(Pixels[1].tile.old_tile.position))
+
+    --global.current_surface.set_tiles({{position = global.all_pixels[global.current_surface.name][1].diode.position, name = "pixel-blue"}}, true)
     end
 end
 
@@ -22,7 +50,20 @@ function initialize()
         global.all_pixels[surface.name] = {}
     end
 
-    global.current_surface = game.surfaces[1].name
+    global.current_surface = game.surfaces[1]
+
+
+    Surface = global.current_surface
+    Pixels = global.all_pixels
+end
+
+function setup_global_variables() 
+    --This is meant for 3 specific reasons and only 3:
+        --re-register conditional event handlers
+        --re-setup meta tables
+        --create local references to tables stored in the global table
+    Surface = global.current_surface
+    Pixels = global.all_pixels
 end
 
 
@@ -41,8 +82,13 @@ script.set_event_filter(defines.events.on_cancelled_deconstruction, pixel_event_
 
 -- Game event handlers
 script.on_event(defines.events.on_built_entity, on_pixel_built)
+
+script.on_event(defines.events.on_pre_player_mined_item, on_pixel_removed)
+
 script.on_event(defines.events.on_tick, on_regular_check)
+
 script.on_init(initialize)
+script.on_load(setup_global_variables)
 
 --[[ TODO
 on_surface_cleared	Called just after a surface is cleared (all entities removed and all chunks deleted).
@@ -83,3 +129,11 @@ type LuaEntityDeconstructionCancelledEventFilters
 type LuaPlayerBuiltEntityEventFilters
 type LuaPlayerMinedEntityEventFilters
 ]]--
+
+
+---
+--[[function table.removekey(table, key)
+    local element = table[key]
+    table[key] = nil
+    return element
+end]]
